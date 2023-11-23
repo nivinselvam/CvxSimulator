043cff5d5a56de0d3129f90c8d1f89c6fcdb3c38
Nivin Selvam P <NivinP1@verifone.com>
2023-11-23 20:15:49 +0530

    inital folder structure creation

--- src/main/java/com/base/Main.java
+++ src/main/java/com/base/Main.java
@@ -1,3 +1,8 @@
+/*
+Author  : Nivin
+Date    : 23/November/2023
+ */
+
 package com.base;
 
 import org.slf4j.Logger;
@@ -6,6 +11,7 @@ import org.slf4j.LoggerFactory;
 public class Main {
 
     public static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
 
     }
